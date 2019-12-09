765fa63c7cea35f0aec1e2f754883bcba4d6d31a
coding-or-afk <seodw1107@hanmail.net>
2019-12-09 14:27:53 +0900

    Add files via upload

---
+++ sdwExeShortCut/sdwExeShortCut/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace sdwExeShortCut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.lvExplorer = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDir = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.runAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nonUACToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uACToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.tableLayoutPanel2);
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1024, 606);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.lvExplorer, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 41);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1017, 562);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // lvExplorer
+            // 
+            this.lvExplorer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvExplorer.LargeImageList = this.imageList1;
+            this.lvExplorer.Location = new System.Drawing.Point(3, 3);
+            this.lvExplorer.MultiSelect = false;
+            this.lvExplorer.Name = "lvExplorer";
+            this.lvExplorer.Size = new System.Drawing.Size(1011, 556);
+            this.lvExplorer.TabIndex = 0;
+            this.lvExplorer.UseCompatibleStateImageBehavior = false;
+            this.lvExplorer.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lvExplorer_MouseClick);
+            this.lvExplorer.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvExplorer_MouseDoubleClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folderopened_yellow.ico");
+            this.imageList1.Images.SetKeyName(1, "hard_drive.ico");
+            this.imageList1.Images.SetKeyName(2, "hard_drive_windows_2.ico");
+            this.imageList1.Images.SetKeyName(3, "Hopstarter-Sleek-Xp-Basic-Document-Blank.ico");
+            this.imageList1.Images.SetKeyName(4, "opened_folder.ico");
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnAdd, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnDelete, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnDir, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1017, 30);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(74, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(83, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(74, 23);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnDir
+            // 
+            this.btnDir.Location = new System.Drawing.Point(163, 3);
+            this.btnDir.Name = "btnDir";
+            this.btnDir.Size = new System.Drawing.Size(74, 23);
+            this.btnDir.TabIndex = 1;
+            this.btnDir.Text = "Open";
+            this.btnDir.UseVisualStyleBackColor = true;
+            this.btnDir.Click += new System.EventHandler(this.btnDir_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runAsToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // runAsToolStripMenuItem
+            // 
+            this.runAsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nonUACToolStripMenuItem,
+            this.uACToolStripMenuItem});
+            this.runAsToolStripMenuItem.Name = "runAsToolStripMenuItem";
+            this.runAsToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.runAsToolStripMenuItem.Text = "Run As...";
+            // 
+            // nonUACToolStripMenuItem
+            // 
+            this.nonUACToolStripMenuItem.Name = "nonUACToolStripMenuItem";
+            this.nonUACToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.nonUACToolStripMenuItem.Text = "Non UAC";
+            this.nonUACToolStripMenuItem.Click += new System.EventHandler(this.nonUACToolStripMenuItem_Click);
+            // 
+            // uACToolStripMenuItem
+            // 
+            this.uACToolStripMenuItem.Name = "uACToolStripMenuItem";
+            this.uACToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.uACToolStripMenuItem.Text = "UAC";
+            this.uACToolStripMenuItem.Click += new System.EventHandler(this.uACToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1049, 631);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ListView lvExplorer;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDir;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem runAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nonUACToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uACToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
+
---
+++ sdwExeShortCut/sdwExeShortCut/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace sdwExeShortCut
+{
+    public partial class Form1 : Form
+    {
+        public string mPropertyValue = String.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeByConfig();
+        }
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "C:\\";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                ListViewItem lvi = new ListViewItem();
+                Icon icon = Icon.ExtractAssociatedIcon(ofd.FileName);
+                Image iconImg = icon.ToBitmap();
+                imageList1.Images.Add(iconImg);
+                lvi.ImageIndex = imageList1.Images.Count-1;
+                lvi.Text = ofd.SafeFileName;
+                lvi.Tag = ofd.FileName;
+                lvExplorer.Items.Add(lvi);
+
+                saveConfig(ofd.SafeFileName,ofd.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Error Occured When Opening File");
+                return;
+            }
+        }
+        
+        private void lvExplorer_DoubleClick(object sender, EventArgs e)
+        {
+            string excPath = Convert.ToString(lvExplorer.SelectedItems[0].Tag);
+            Process proc = new Process();
+            proc.StartInfo.UseShellExecute = true;
+            proc.StartInfo.FileName = excPath;
+            proc.StartInfo.CreateNoWindow = false;
+            try
+            {
+                proc.Start();
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message);
+                return;
+            }
+            
+        }
+
+        private void btnDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    if(files[i].Length < 4)
+                    {
+                        return;
+                    }
+
+                    if(files[i].Substring(files[i].Length - 4, 4) == ".exe")
+                    {
+                        ListViewItem lvi = new ListViewItem();
+                        Icon icon = Icon.ExtractAssociatedIcon(files[i]);
+                        Image iconImg = icon.ToBitmap();
+                        imageList1.Images.Add(iconImg);
+                        lvi.ImageIndex = imageList1.Images.Count - 1;
+                        lvi.Text = strchr(files[i],'\\');
+                        lvi.Tag = files[i];
+                        lvExplorer.Items.Add(lvi);
+
+                        saveConfig(lvi.Text,files[i]);
+                    }
+                }
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Directory Not Found");
+                return;
+            }
+        }
+
+        private string strchr(string origin, char charToSearch)
+        {
+            int? found = origin.LastIndexOf(charToSearch);
+            if(found > -1)
+            {
+                string retval = origin.Substring((int)found+1, origin.Length - (int)found-1);
+                return retval;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void lvExplorer_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // 선택된 아이템이 없을때
+            if (lvExplorer.SelectedItems.Count < 1)
+            {
+                return;
+            }
+
+            //오른쪽 더블클릭
+            if(e.Button == MouseButtons.Right)
+            {
+                return;
+            }
+
+            string excPath = Convert.ToString(lvExplorer.SelectedItems[0].Tag);
+            Process proc = new Process();
+            proc.StartInfo.UseShellExecute = true;
+            proc.StartInfo.FileName = excPath;
+            try
+            {
+                proc.Start();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                return;
+            }
+        }
+
+        private void lvExplorer_MouseClick(object sender, MouseEventArgs e)
+        {
+            //선택된 아이템이 없을때
+            if(lvExplorer.SelectedItems.Count < 1)
+            {
+                return;
+            }
+
+            //오른쪽 버튼 클릭
+            if (e.Button == MouseButtons.Right)
+            {
+                if (lvExplorer.FocusedItem.Bounds.Contains(e.Location))
+                {
+                    contextMenuStrip1.Show(Cursor.Position);
+                }
+            }
+        }
+
+        private void uACToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string excPath = Convert.ToString(lvExplorer.SelectedItems[0].Tag);
+            Process proc = new Process();
+            proc.StartInfo.FileName = excPath;
+            proc.StartInfo.UseShellExecute = true;
+            if (System.Environment.OSVersion.Version.Major >= 6)
+            {
+                proc.StartInfo.Verb = "runas";
+            }
+            try
+            {
+                proc.Start();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                return;
+            }
+        }
+        private void nonUACToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string excPath = Convert.ToString(lvExplorer.SelectedItems[0].Tag);
+            Process proc = new Process();
+            proc.StartInfo.UseShellExecute = true;
+            proc.StartInfo.FileName = excPath;
+            try
+            {
+                proc.Start();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                return;
+            }
+        }
+
+        public void InitializeByConfig()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                for(int i=0; i< appSettings.Count; i++)
+                {
+                    string filepath = appSettings[i];
+                    ListViewItem lvi = new ListViewItem();
+                    Icon icon = Icon.ExtractAssociatedIcon(filepath);
+                    Image iconImg = icon.ToBitmap();
+                    imageList1.Images.Add(iconImg);
+                    lvi.ImageIndex = imageList1.Images.Count - 1;
+                    lvi.Text = strchr(filepath, '\\');
+                    lvi.Tag = filepath;
+                    lvExplorer.Items.Add(lvi);
+                }
+            }
+            catch (ConfigurationException err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        public void saveConfig(string key, string path)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var appSettings = configFile.AppSettings.Settings;
+                if (appSettings[key] == null)
+                {
+                    appSettings.Add(key, path);
+                }
+                else
+                {
+                    MessageBox.Show("Already Exists!");
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        public void deleteConfig(string key)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var appSettings = configFile.AppSettings.Settings;
+                if (appSettings[key] == null)
+                {
+                    MessageBox.Show("Error in Deleting Config File");
+                }
+                else
+                {
+                    appSettings.Remove(key);
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException err)
+            {
+                MessageBox.Show(err.Message);
+            }
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            deleteItem();
+        }
+
+        private void deleteItem()
+        {
+            
+            if(lvExplorer.SelectedItems.Count > 0)
+            {
+                string key = strchr((string)lvExplorer.SelectedItems[0].Tag, '\\');
+                deleteConfig(key);
+                lvExplorer.Items.Remove(lvExplorer.SelectedItems[0]);
+            }
+            //선택된 아이템이 없음
+            else
+            {
+                MessageBox.Show("No Items Selected");
+                return;
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            deleteItem();
+        }
+    }
+}
